a831ef6fbb206581b758b728c252a9a9c602aeee
David Bukovšek <david.bukovsek@gmail.com>
2017-03-14 20:57:29 +0100

    Smartninja forum gae application

--- utils/special_functions.py
+++ utils/special_functions.py
@@ -13,25 +13,6 @@ def is_local():
         return False
 
 
-# def check_csrf_protection(func):
-#     def func_wrapper(some_object):
-#         csrf_token = self.request.get("csrf_token") # to je vrednost v skritem input polju
-#         csrf_value = memcache.get(csrf_token) # dobimo uporabnikov email
-#         if str(csrf_value) != user.email(): # trenutni email in email v csrf_value se morata ujemati
-#             return self.write("You are hacker!")
-
-# def check_csrf_protection(user):
-#     def tags_decorator(func):
-#         def func_wrapper(self):
-#             csrf_token = self.request.get("csrf_token")  # to je vrednost v skritem input polju
-#             csrf_value = memcache.get(csrf_token)  # dobimo uporabnikov email
-#             if str(csrf_value) != user.email():  # trenutni email in email v csrf_value se morata ujemati
-#                 return self.write("You are hacker!")
-#         return func_wrapper
-#     return tags_decorator
-
-
-
 def check_csrf_protection(handler):
     def func_wrapper(self, *args, **kwargs):
         csrf_token = self.request.get("csrf_token")
